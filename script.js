ab83787f01c1f11a46733d60bacd4a362828d98c
albart67 <nouvovnus@hotmail.fr>
2021-01-16 22:58:38 +0100

    2 files modified, 9 deleted

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello!')
+console.log("hello!");
+
+let removeCartItemButtons = document.getElementsByClassName("btn-danger");
+console.log(removeCartItemButtons);
+console.log(removeCartItemButtons[0]);
+
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+  let button = removeCartItemButtons[i];
+  console.log(button);
+  button.addEventListener("click", function(event) {
+    console.log("clicked");
+    let buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+  });
+}
+
+console.log(removeCartItemButtons.length);
