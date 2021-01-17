18d0507e2ddfeca29e6b683162d32a58358aef15
ALAIN BARTHELME <60420847+albart67@users.noreply.github.com>
2021-01-17 19:01:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,19 @@ function ready() {
     var button = addToCartButtons[i];
     button.addEventListener("click", addToCartClicked);
   }
+
+  document
+    .getElementsByClassName("btn-purchase")[0]
+    .addEventListener("click", purchaseClicked);
+}
+
+function purchaseClicked() {
+  alert("Merci pour votre achat !");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+  updateCartTotal();
 }
 
 function removeCartItem(event) {
@@ -48,29 +61,43 @@ function addToCartClicked(event) {
   var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
   console.log(title, price, imageSrc);
   addItemToCart(title, price, imageSrc);
+  updateCartTotal();
 }
 
 function addItemToCart(title, price, imageSrc) {
   var cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
   var cartItems = document.getElementsByClassName("cart-items")[0];
+  var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert("Ce produit est déja présent dans le panier");
+      return;
+    }
+  }
   var cartRowContents = ` 
-      <div class="cart-row">
-          <div class="cart-item cart-column">
+       <div class="cart-item cart-column">
             <img
               class="cart-item-image"
-              src="https://github.com/albart67/boutique-stackblitz1/blob/master/Images/Shirt.png?raw=true"
+              src="${imageSrc}"
               width="100"
               height="100"
             />
-            <span class="cart-item-title">T-Shirt</span>
+            <span class="cart-item-title">${title}</span>
           </div>
-          <span class="cart-price cart-column">$19.99</span>
+          <span class="cart-price cart-column">${price}</span>
           <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1" />
             <button class="btn btn-danger" type="button">REMOVE</button>
       </div>`;
   cartRow.innerHTML = cartRowContents;
   cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName("btn-danger")[0]
+    .addEventListener("click", removeCartItem);
+  cartRow
+    .getElementsByClassName("cart-quantity-input")[0]
+    .addEventListener("change", quantityChanged);
 }
 
 function updateCartTotal() {
